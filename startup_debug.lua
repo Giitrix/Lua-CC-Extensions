df7c88419a3246d8a66a799f0b97e4e5d351ec41
Grigoriy <giit@pm.me>
2022-09-17 01:39:20 +0300

    Debug moved to another dir.

---
+++ startup_debug.lua
@@ -0,0 +1,17 @@
+ccemux.attach("left", "wireless_modem", {
+    -- The range of this modem
+    range = 64,
+
+    -- Whether this is an ender modem
+    interdimensional = false,
+
+    -- The current world's name. Sending messages between worlds requires an interdimensional modem
+    world = "main",
+
+    -- The position of this wireless modem within the world
+    posX = 0, posY = 0, posZ = 0,
+})
+
+
+
+shell.run("startup-scripts")
